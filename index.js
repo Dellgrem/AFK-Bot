56e1ca33d790a0194cab662b094074d4b1371278
Jim Barkov <ellrinpointer@gmail.com>
2022-10-29 22:45:58 +0200

    Added keep-alive system.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const mineflayer = require('mineflayer')
 const AutoAuth = require('mineflayer-auto-auth')
+const keepAlive = require('./server');
+const Monitor = require('ping-monitor');
+
+keepAlive();
+const monitor = new Monitor({
+    website: '',
+    title: 'Bot Keep-Alive',
+    interval: 2
+});
+
+monitor.on('up', (res) => console.log(`${res.website} its on.`));
+monitor.on('down', (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
+monitor.on('stop', (website) => console.log(`${website} has stopped.`) );
+monitor.on('error', (error) => console.log(error));
 
 const bot = mineflayer.createBot({
   host: process.env.SERVER_HOST,
