3c5f41edbd195939b9e75b9fa8ebbe2b259a2383
Jim Barkov <ellrinpointer@gmail.com>
2022-10-30 08:22:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-const mineflayer = require('mineflayer')
-const AutoAuth = require('mineflayer-auto-auth')
-const keepAlive = require('./server');
-const Monitor = require('ping-monitor');
+const mineflayer = require("mineflayer");
+const AutoAuth = require("mineflayer-auto-auth");
+const antiafk = require("mineflayer-antiafk");
+const keepAlive = require("./server");
+const Monitor = require("ping-monitor");
 
 keepAlive();
 const monitor = new Monitor({
-    website: '',
-    title: 'Bot Keep-Alive',
-    interval: 2
+  website: "",
+  title: "Bot Keep-Alive",
+  interval: 2,
 });
 
-monitor.on('up', (res) => console.log(`${res.website} its on.`));
-monitor.on('down', (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
-monitor.on('stop', (website) => console.log(`${website} has stopped.`) );
-monitor.on('error', (error) => console.log(error));
+monitor.on("up", (res) => console.log(`${res.website} its on.`));
+monitor.on("down", (res) =>
+  console.log(`${res.website} it has died - ${res.statusMessage}`)
+);
+monitor.on("stop", (website) => console.log(`${website} has stopped.`));
+monitor.on("error", (error) => console.log(error));
 
-function createBot () {
+function createBot() {
   const bot = mineflayer.createBot({
-  host: process.env.SERVER_HOST,
-  username: process.env.BOT_USERNAME,
-  port: process.env.SERVER_PORT,
-  version: process.env.BOT_VERSION,
-  plugins: [AutoAuth],
-  AutoAuth: process.env.BOT_PASSWORD
-  })
+    host: process.env.SERVER_HOST,
+    username: process.env.BOT_USERNAME,
+    port: process.env.SERVER_PORT,
+    version: process.env.BOT_VERSION,
+    plugins: [AutoAuth],
+    AutoAuth: process.env.BOT_PASSWORD,
+  });
 
-  bot.on('login', () => {
-    console.log("Connected :D")
-  })
+  bot.loadPlugin(antiafk);
 
-  bot.on('kicked', console.log)
-  bot.on('error', console.log)
-  bot.on('end', createBot)
+  bot.on("spawn", () => {
+    bot.afk.setOptions({ fishing: false });
+    bot.afk.start();
+  });
+
+  bot.on("login", () => {
+    console.log("Connected :D");
+  });
+
+  bot.on("kicked", console.log);
+  bot.on("error", console.log);
+  bot.on("end", createBot);
 }
 
-createBot()
+createBot();
